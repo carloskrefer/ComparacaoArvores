4500b0b507a5bf8b4595fb3f599d17ece2d9f99d
Carlos Krefer <carloskrefer@hotmail.com>
2023-10-13 12:06:55 -0300

    Commit

--- Main.java
+++ Main.java
@@ -5,18 +5,17 @@ public class Main {
 	public static void main(String[] args) {
 		ArvoreBinariaAVL a = new ArvoreBinariaAVL();
 		
-		a.inserir(10);
-		a.inserir(5);
-		a.inserir(15);
-		a.inserir(3);
-		a.inserir(7);
-		a.inserir(13);
-		a.inserir(17);
-		a.inserir(12);
-		a.inserir(14);
-		a.inserir(16);
-		a.inserir(18);
 		a.inserir(20);
+		a.inserir(15);
+		a.inserir(25);
+		a.inserir(10);
+		a.inserir(17);
+		a.inserir(30);
+		a.inserir(5);
+		a.inserir(12);
+		a.inserir(18);
+		a.inserir(16);
+		a.inserir(19);
 
 		a.imprimir();
 	}
